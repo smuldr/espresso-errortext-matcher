72ce8e0f85de3bd70bd80000666044594ff32444
Steven Mulder <steven.mulder@gmail.com>
2015-07-24 17:16:21 +0200

    Show ActionBar in LoginActivity.
    Pretty screenshots matter.

--- app/src/main/java/smuldr/espresso/errormatcher/LoginActivity.java
+++ app/src/main/java/smuldr/espresso/errormatcher/LoginActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -28,7 +29,7 @@ import java.util.List;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
   /**
    * A dummy authentication store containing known user names and passwords.
